2c711225111fb194a2ee52b7c83f8d1ad51eed49
frederik-kofoed-marqversen <fkm@kvantify.dk>
2023-02-28 11:04:06 +0100

    added the exercise "complex" and the dependencies

---
+++ exercises/complex/main.rs
@@ -0,0 +1,17 @@
+extern crate num_traits;
+use num_complex::Complex;
+const I: Complex<f64> = Complex::<f64>::new(0.0, 1.0);
+const ONE: Complex<f64> = Complex::<f64>::new(1.0, 0.0);
+const PI: f64 = std::f64::consts::PI;
+
+fn main() {
+    println!("sqrt(-1) = {}", (-ONE).sqrt());
+    println!("sqrt(i) = {}", I.sqrt());
+    println!("e^i = {}", I.exp());
+    println!("e^(i * pi) = {}", (I*PI).exp());
+    println!("i^i = {}", I.powc(I));
+    println!("ln(i) = {}", I.ln());
+    println!("sin(i * pi) = {}", (I*PI).sin());
+    println!("sinh(i * pi) = {}", (I*PI).sinh());
+    println!("cosh(i * pi) = {}", (I*PI).cosh());
+}
