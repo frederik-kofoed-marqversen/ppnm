54c518a2ba8a3c1e5e13e5c4f5263d74aaf5d099
frederik-kofoed-marqversen <fkm@kvantify.dk>
2023-03-09 16:17:47 +0100

    simplified constructors

--- libraries/matrix/src/matrix/constructors.rs
+++ libraries/matrix/src/matrix/constructors.rs
@@ -1,41 +1,25 @@
 use super::Matrix;
-use std::convert::TryFrom;
-impl<T: TryFrom<u8> + Copy> Matrix<T> {
+
+impl<T: From<u8> + Copy> Matrix<T> {
     pub fn zeros(num_rows: usize, num_cols: usize) -> Self {
-        if let Ok(zero) = T::try_from(0) {
-            let data: Vec<T> = vec![zero; num_rows * num_cols];
-            Self::from_data(data, num_rows, num_cols)
-        } else {
-            panic!("Could not cast value 0")
-        }
+        let data: Vec<T> = vec![T::from(0); num_rows * num_cols];
+        Self::from_data(data, num_rows, num_cols)
     }
 
     pub fn ones(num_rows: usize, num_cols: usize) -> Self {
-        if let Ok(one) = T::try_from(1) {
-            let data: Vec<T> = vec![one; num_rows * num_cols];
-            Self::from_data(data, num_rows, num_cols)
-        } else {
-            panic!("Could not cast value 1")
-        }
+        let data: Vec<T> = vec![T::from(1); num_rows * num_cols];
+        Self::from_data(data, num_rows, num_cols)
     }
 
     pub fn idty(dim: usize) -> Self {
-        if let Ok(one) = T::try_from(1) {
-            let mut result = Self::zeros(dim, dim);
-            for i in 0..dim {result.data[(dim + 1) * i] = one};
+        let mut result = Self::zeros(dim, dim);
+            for i in 0..dim {result.data[(dim + 1) * i] = T::from(1)};
             return result;
-        } else {
-            panic!("Could not cast value 1")
-        }
     }
 
     pub fn zeros_like(matrix: &Self) -> Self {
         Self::zeros(matrix.num_rows, matrix.num_cols)
     }
-
-    // pub fn norm(&self) -> f64 {
-    //     f64::sqrt(self.iter().map(|x| x*x).sum())
-    // }
 }
 
 #[cfg(test)]
