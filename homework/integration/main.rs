fd8efdd98bf0bac4ad2bc039f37d96e69b69dd80
frederik-kofoed-marqversen <fkm@kvantify.dk>
2023-03-31 12:39:15 +0200

    inifinite limit transformations for scientific::integration::integrate

--- homework/integration/main.rs
+++ homework/integration/main.rs
@@ -1,55 +1,36 @@
 extern crate sfuns;
+extern crate scientific;
 
 use sfuns::map_range;
+use scientific::integration::*;
 use std::f64::consts::PI;
 use std::io::Write;
 
-pub fn integrate(f: &mut impl FnMut(f64) -> f64, a: f64, b: f64, precision: Option<(f64, f64)>) -> f64 {
-    let (abs, rel) = precision.unwrap_or((0.01, 0.01));
-    let (f2, f3) = ((f)(a+2.0/6.0*(b-a)), (f)(a+4.0/6.0*(b-a)));
-    return integrate_recursive(f, a, b, abs, rel, f2, f3);
-}
-
-fn integrate_recursive(f: &mut impl FnMut(f64) -> f64, a: f64, b: f64, abs:f64, rel:f64, f2: f64, f3: f64) -> f64 {
-    let (f1, f4) = ((f)(a+1.0/6.0*(b-a)), (f)(a+5.0/6.0*(b-a)));
-    let int = (2.0*f1+f2+f3+2.0*f4) / 6.0 * (b-a); // higher order rule: trapezeum
-    let int2 = (    f1+f2+f3+    f4) / 4.0 * (b-a); // lower order rule: rectangular
-    let error = (int - int2).abs();
-    if error <= f64::max(abs, rel*int.abs()) {
-        return int;
-    } else {
-        return integrate_recursive(f, a, (a+b)/2.0, abs/f64::sqrt(2.0), rel, f1, f2)
-             + integrate_recursive(f, (a+b)/2.0, b, abs/f64::sqrt(2.0), rel, f3, f4);
-    }
-}
-
-pub fn clenshaw_curtis(f: &mut impl FnMut(f64) -> f64, a: f64, b: f64, precision: Option<(f64, f64)>) -> f64 {
-    let mut ff = |theta: f64| {
-        (f)((a+b)/2.0 + (b-a)/2.0*f64::cos(theta)) * f64::sin(theta) * (b-a)/2.0
-    };
-    return integrate(&mut ff, 0.0, std::f64::consts::PI, precision);
-}
-
-pub fn integrate_improper(f: &mut impl FnMut(f64) -> f64, a: f64, b: f64, precision: Option<(f64, f64)>) -> f64 {
-    match (a.is_infinite(), b.is_infinite()) {
-        (true, true) => {
-            let mut ff = |t: f64| (f)(t/(1.0-t*t)) * (1.0+t*t)/(1.0-t*t).powi(2);
-            return integrate(&mut ff, -1.0, 1.0, precision);
-        },
-        (false, true) => {
-            let mut ff = |t: f64| (f)(a + (1.0-t)/t) / (t*t);
-            return integrate(&mut ff, 0.0, 1.0, precision);
-        },
-        (true, false) => {
-            let mut ff = |t: f64| (f)(b - (1.0-t)/t) / (t*t);
-            return integrate(&mut ff, 0.0, 1.0, precision);
-        },
-        (false, false) => return integrate(f, a, b, precision),
-    };
-}
-
 fn erf(z: f64) -> f64 {
-    integrate(&mut |x: f64| f64::exp(-x*x) * 2.0 / f64::sqrt(PI), 0.0, z, Some((0.0001, 0.0)))
+    integrate(&|x: f64| f64::exp(-x*x) * 2.0 / f64::sqrt(PI), 0.0, z, Some((0.0001, 0.0))).0
+}
+
+fn print_test(file: &mut std::fs::File, f: impl Fn(f64) -> f64, a: f64, b: f64, precision: Option<(f64, f64)>, expected: f64, do_cc: bool) -> std::io::Result<()> {
+    let (int, _err, evals) = integrate(&f, a, b, precision);
+    
+    file.write(format!(
+        "Integrate -> {int}\n\
+        \tAbsolute accuracy < 0.01: {}\n\
+        \tIntegrand evaluations: {evals}\n",
+        (int - expected).abs() < 0.01,
+    ).as_bytes())?;
+
+    if do_cc {
+        let (int, _err, evals) = clenshaw_curtis(&f, a, b, precision);
+        file.write(format!(
+            "Clenshaw Curtis -> {int}\n\
+            \tAbsolute accuracy < 0.01: {}\n\
+            \tIntegrand evaluations: {evals}\n",
+            (int - expected).abs() < 0.01,
+        ).as_bytes())?;
+    }
+
+    Ok(())
 }
 
 fn main() -> std::io::Result<()> {
@@ -61,122 +42,59 @@ fn main() -> std::io::Result<()> {
         file.write(format!("{z}\t{}\n", erf(z)).as_bytes())?;
     }
     
-    // count integrand evaluations
+    // Output file
     let mut file = std::fs::File::create("Out.txt")?;
+    file.write("RESULTS FOR INTEGRATION HOMEWORK\n".as_bytes())?;
 
-    // f(x)=1/√(x)
-    file.write("Integrand evaluations for f(x)=1/√(x)\n".as_bytes())?;
-    let mut count: usize = 0;
-    let mut f = |x| {
-        count += 1;
-        return 1.0/f64::sqrt(x);
-    };
-    integrate(&mut f, 0.0, 1.0, None);
-    let string = format!("\tintegrate(f, 0, 1): {}\n", count);
-    file.write(string.as_bytes())?;
+    let (a, b) = (0.0, 1.0);
     
-    let mut count: usize = 0;
-    let mut f = |x| {
-        count += 1;
-        return 1.0/f64::sqrt(x);
-    };
-    clenshaw_curtis(&mut f, 0.0, 1.0, None);
-    let string = format!("\tclenshaw_curtis(f, 0, 1): {}\n", count);
-    file.write(string.as_bytes())?;
+    // Function 1
+    let f = |x: f64| f64::sqrt(x);
+    let expected = 2.0/3.0;
+    file.write(format!("\n∫_0^1 dx √(x) = 2/3 = {expected}\n").as_bytes())?;
+    print_test(&mut file, f, a, b, None, expected, true)?;
 
-    // f(x)=ln(x)/√(x)
-    file.write("\nIntegrand evaluations for f(x)=ln(x)/√(x)\n".as_bytes())?;
-    let mut count: usize = 0;
-    let mut f = |x| {
-        count += 1;
-        return f64::ln(x)/f64::sqrt(x);
-    };
-    integrate(&mut f, 0.0, 1.0, None);
-    let string = format!("\tintegrate(f, 0, 1): {}\n", count);
-    file.write(string.as_bytes())?;
-    
-    let mut count: usize = 0;
-    let mut f = |x| {
-        count += 1;
-        return f64::ln(x)/f64::sqrt(x);
-    };
-    clenshaw_curtis(&mut f, 0.0, 1.0, None);
-    let string = format!("\tclenshaw_curtis(f, 0, 1): {}\n", count);
-    file.write(string.as_bytes())?;
+    // Function 2
+    let f = |x: f64| 1.0/f64::sqrt(x);
+    let expected = 2.0;
+    file.write(format!("\n∫_0^1 dx 1/√(x) = {expected}\n").as_bytes())?;
+    print_test(&mut file, f, a, b, None, expected, true)?;
 
+    // Function 3
+    let f = |x: f64| 4.0 * f64::sqrt(1.0 - x*x);
+    let expected = PI;
+    file.write(format!("\n∫_0^1 dx 4√(1-x²) = π = {expected}\n").as_bytes())?;
+    print_test(&mut file, f, a, b, None, expected, true)?;
 
+    // Function 4
+    let f = |x: f64| f64::ln(x)/f64::sqrt(x);
+    let expected = -4.0;
+    file.write(format!("\n∫_0^1 dx 1n(x)/√(x) = {expected}\n").as_bytes())?;
+    print_test(&mut file, f, a, b, Some((0.01, 0.0)), expected, true)?;
+
+    // Integral representation of arctan
+    let f = |x: f64| 1.0 / (1.0 + x * x);
+    let expected = PI/2.0;
+    file.write(format!("\n∫_0^∞ dx 1/(1+x^2) = π/2 = {expected}\n").as_bytes())?;
+    print_test(&mut file, f, 0.0, f64::INFINITY, None, expected, false)?;
+
+    // Polynomial improper integral
+    let f = |x: f64| x.powi(-2);
+    let expected = 1.0;
+    file.write(format!("\n∫_1^∞ dx 1/x^2 = {expected}\n").as_bytes())?;
+    print_test(&mut file, f, 1.0, f64::INFINITY, None, expected, false)?;
+
+    // The Gaussian integral
+    let f = |x: f64| f64::exp(-x*x);
+    let expected = PI.sqrt();
+    file.write(format!("\n∫_(-∞)^∞ dx e^(-x^2) = √π = {expected}\n").as_bytes())?;
+    print_test(&mut file, f, f64::NEG_INFINITY, f64::INFINITY, None, expected, false)?;
+
+    // Integral representation of the Gamma function
+    let f = |x: f64| x.powi(4) * f64::exp(-x);
+    let expected = 24.0;
+    file.write(format!("\n∫_0^∞ dx x^4 * e^(-x) = Γ(4 + 1) = 4! = {expected}\n").as_bytes())?;
+    print_test(&mut file, f, 0.0, f64::INFINITY, None, expected, false)?;
 
     return Ok(());
-}
-
-#[cfg(test)]
-mod tests {
-    #![allow(non_snake_case)]
-    use super::*;
-    
-    #[test]
-    fn integral_A1() {
-        let mut f = |x: f64| f64::sqrt(x);
-        let i = integrate(&mut f, 0.0, 1.0, None);
-        assert!((i - 2.0/3.0).abs() < 0.01);
-    }
-
-    #[test]
-    fn integral_A2() {
-        let mut f = |x: f64| 1.0/f64::sqrt(x);
-        let i = integrate(&mut f, 0.0, 1.0, None);
-        assert!((i - 2.0).abs() < 0.01);
-    }
-    
-    #[test]
-    fn integral_A3() {
-        let mut f = |x: f64| 4.0 * f64::sqrt(1.0 - x*x);
-        let i = integrate(&mut f, 0.0, 1.0, Some((0.01, 0.0)));
-        assert!((i - std::f64::consts::PI).abs() < 0.01);
-    }
-
-    #[test]
-    fn integral_A4() {
-        let mut f = |x: f64| f64::ln(x)/f64::sqrt(x);
-        let i = integrate(&mut f, 0.0, 1.0, None);
-        assert!((i - (-4.0)).abs() < 0.01);
-    }
-
-    #[test]
-    fn integral_B1() {
-        let mut f = |x: f64| 1.0/f64::sqrt(x);
-        let i = clenshaw_curtis(&mut f, 0.0, 1.0, None);
-        assert!((i - 2.0).abs() < 0.01);
-    }
-
-    #[test]
-    fn integral_B2() {
-        let mut f = |x: f64| f64::ln(x)/f64::sqrt(x);
-        let i = clenshaw_curtis(&mut f, 0.0, 1.0, Some((0.01, 0.0)));
-        assert!((i - (-4.0)).abs() < 0.01);
-    }
-
-    #[test]
-    fn improper_polynomial_integral() {
-        // x**(-2)
-        let mut f = |x: f64| x.powi(-2);
-        let i = integrate_improper(&mut f, 1.0, f64::INFINITY, None);
-        assert!((i - 1.0).abs() < 0.01);
-    }
-
-    #[test]
-    fn gaussian_integral() {
-        // e**(-x**2)
-        let mut f = |x: f64| f64::exp(-x*x);
-        let i = integrate_improper(&mut f, f64::NEG_INFINITY, f64::INFINITY, None);
-        assert!((i - PI.sqrt()).abs() < 0.01);
-    }
-
-    #[test]
-    fn gamma_function() {
-        // 4! = gamma(5 - 1)
-        let mut f = |x: f64| x.powi(5-1) * f64::exp(-x);
-        let i = integrate_improper(&mut f, 0.0, f64::INFINITY, None);
-        assert!((i - 24.0).abs() < 0.01);
-    }
 }
--- libraries/scientific/src/integration.rs
+++ libraries/scientific/src/integration.rs
@@ -1,47 +1,59 @@
-pub fn integrate(f: &impl Fn(f64) -> f64, a: f64, b: f64, precision: Option<(f64, f64)>) -> f64 {
-    let (abs, rel) = precision.unwrap_or((0.01, 0.01));
-    let (f2, f3) = ((f)(a+2.0/6.0*(b-a)), (f)(a+4.0/6.0*(b-a)));
-    return integrate_recursive(f, a, b, abs, rel, f2, f3);
+pub fn integrate(f: &impl Fn(f64) -> f64, a: f64, b: f64, precision: Option<(f64, f64)>) -> (f64, f64, u32) {
+    match (a.is_infinite(), b.is_infinite()) {
+        (true, true) => {
+            if a == b {panic!("Bad integral limits")};
+            let ff = |t: f64| (f)(t/(1.0-t*t)) * (1.0+t*t)/(1.0-t*t).powi(2);
+            return recursive_adaptive_integrator(&ff, -1.0, 1.0, precision);
+        },
+        (false, true) => {
+            if b < 0.0 {
+                let (int, error, evals) = integrate(f, b, a, precision); 
+                return (-int, error, evals);
+            };
+            let ff = |t: f64| (f)(a + (1.0-t)/t) / (t*t);
+            return recursive_adaptive_integrator(&ff, 0.0, 1.0, precision);
+        },
+        (true, false) => {
+            if a > 0.0 {
+                let (int, error, evals) = integrate(f, b, a, precision); 
+                return (-int, error, evals);
+            };
+            let ff = |t: f64| (f)(b - (1.0-t)/t) / (t*t);
+            return recursive_adaptive_integrator(&ff, 0.0, 1.0, precision);
+        },
+        (false, false) => {
+            return recursive_adaptive_integrator(f, a, b, precision);
+        },
+    };
 }
 
-fn integrate_recursive(f: &impl Fn(f64) -> f64, a: f64, b: f64, abs:f64, rel:f64, f2: f64, f3: f64) -> f64 {
+pub fn recursive_adaptive_integrator(f: &impl Fn(f64) -> f64, a: f64, b: f64, precision: Option<(f64, f64)>) -> (f64, f64, u32) {
+    let (abs, rel) = precision.unwrap_or((0.01, 0.01));
+    let (f2, f3) = ((f)(a+2.0/6.0*(b-a)), (f)(a+4.0/6.0*(b-a)));
+    let (int, error, evals) = integrate_recursive(&f, a, b, abs, rel, f2, f3);
+    return (int, error, evals+2)
+}
+
+fn integrate_recursive(f: &impl Fn(f64) -> f64, a: f64, b: f64, abs:f64, rel:f64, f2: f64, f3: f64) -> (f64, f64, u32) {
     let (f1, f4) = ((f)(a+1.0/6.0*(b-a)), (f)(a+5.0/6.0*(b-a)));
     let int = (2.0*f1+f2+f3+2.0*f4) / 6.0 * (b-a); // higher order rule: trapezeum
     let int2 = (    f1+f2+f3+    f4) / 4.0 * (b-a); // lower order rule: rectangular
     let error = (int - int2).abs();
     if error <= f64::max(abs, rel*int.abs()) {
-        return int;
+        return (int, error.powi(2), 2);
     } else {
-        return integrate_recursive(f, a, (a+b)/2.0, abs/f64::sqrt(2.0), rel, f1, f2)
-             + integrate_recursive(f, (a+b)/2.0, b, abs/f64::sqrt(2.0), rel, f3, f4);
+        let (int1, error1, evals1) = integrate_recursive(f, a, (a+b)/2.0, abs/f64::sqrt(2.0), rel, f1, f2);
+        let (int2, error2, evals2) = integrate_recursive(f, (a+b)/2.0, b, abs/f64::sqrt(2.0), rel, f3, f4);
+        return (int1+int2, error1+error2, evals1+evals2+2);
     }
 }
 
-pub fn clenshaw_curtis(f: &impl Fn(f64) -> f64, a: f64, b: f64, precision: Option<(f64, f64)>) -> f64 {
+pub fn clenshaw_curtis(f: &impl Fn(f64) -> f64, a: f64, b: f64, precision: Option<(f64, f64)>) -> (f64, f64, u32) {
     let ff = |theta: f64| {
         (f)((a+b)/2.0 + (b-a)/2.0*f64::cos(theta)) * f64::sin(theta) * (b-a)/2.0
     };
-    return integrate(&ff, 0.0, std::f64::consts::PI, precision);
+    return recursive_adaptive_integrator(&ff, 0.0, std::f64::consts::PI, precision);
 }
-
-pub fn integrate_improper(f: &impl Fn(f64) -> f64, a: f64, b: f64, precision: Option<(f64, f64)>) -> f64 {
-    match (a.is_infinite(), b.is_infinite()) {
-        (true, true) => {
-            let ff = |t: f64| (f)(t/(1.0-t*t)) * (1.0+t*t)/(1.0-t*t).powi(2);
-            return integrate(&ff, -1.0, 1.0, precision);
-        },
-        (false, true) => {
-            let ff = |t: f64| (f)(a + (1.0-t)/t) / (t*t);
-            return integrate(&ff, 0.0, 1.0, precision);
-        },
-        (true, false) => {
-            let ff = |t: f64| (f)(b - (1.0-t)/t) / (t*t);
-            return integrate(&ff, 0.0, 1.0, precision);
-        },
-        (false, false) => return integrate(f, a, b, precision),
-    };
-}
-
 
 #[cfg(test)]
 mod tests {
@@ -51,49 +63,49 @@ mod tests {
     #[test]
     fn test_integrate_1() {
         let f = |x: f64| 4.0 * f64::sqrt(1.0 - x*x);
-        let i = integrate(&f, 0.0, 1.0, Some((0.01, 0.0)));
+        let i = integrate(&f, 0.0, 1.0, Some((0.01, 0.0))).0;
         assert!((i - PI).abs() < 0.01);
     }
 
     #[test]
     fn test_integrate_2() {
         let f = |x: f64| 4.0 * f64::sqrt(1.0 - x*x);
-        let i = integrate(&f, 1.0, 0.0, Some((0.01, 0.0)));
+        let i = integrate(&f, 1.0, 0.0, Some((0.01, 0.0))).0;
         assert!((i - (-PI)).abs() < 0.01);
     }
 
     #[test]
     fn test_integrate_3() {
         let f = |x: f64| f64::ln(x)/f64::sqrt(x);
-        let i = integrate(&f, 0.0, 1.0, None);
+        let i = integrate(&f, 0.0, 1.0, None).0;
         assert!((i - (-4.0)).abs() < 0.01);
     }
 
     #[test]
     fn test_clenshaw_curtis_1() {
         let f = |x: f64| 1.0/f64::sqrt(x);
-        let i = clenshaw_curtis(&f, 0.0, 1.0, None);
+        let i = clenshaw_curtis(&f, 0.0, 1.0, None).0;
         assert!((i - 2.0).abs() < 0.01);
     }
 
     #[test]
     fn test_clenshaw_curtis_2() {
         let f = |x: f64| f64::ln(x)/f64::sqrt(x);
-        let i = clenshaw_curtis(&f, 0.0, 1.0, Some((0.01, 0.0)));
+        let i = clenshaw_curtis(&f, 0.0, 1.0, Some((0.01, 0.0))).0;
         assert!((i - (-4.0)).abs() < 0.01);
     }
 
     #[test]
     fn test_improper_1() {
         let f = |x: f64| x.powi(-2);
-        let i = integrate_improper(&f, 1.0, f64::INFINITY, None);
+        let i = integrate(&f, 1.0, f64::INFINITY, None).0;
         assert!((i - 1.0).abs() < 0.01);
     }
 
     #[test]
     fn test_improper_2() {
         let f = |x: f64| f64::exp(-x*x);
-        let i = integrate_improper(&f, f64::NEG_INFINITY, f64::INFINITY, None);
+        let i = integrate(&f, f64::NEG_INFINITY, f64::INFINITY, None).0;
         assert!((i - PI.sqrt()).abs() < 0.01);
     }
 
@@ -101,7 +113,7 @@ mod tests {
     fn test_improper_3() {
         // 4! = gamma(5 - 1)
         let f = |x: f64| x.powi(5-1) * f64::exp(-x);
-        let i = integrate_improper(&f, 0.0, f64::INFINITY, None);
+        let i = integrate(&f, 0.0, f64::INFINITY, None).0;
         assert!((i - 24.0).abs() < 0.01);
     }
 }
